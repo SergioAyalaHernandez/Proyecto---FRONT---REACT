aecf553e0b09e74afe8b39c4388c92a5d567c77d
Jonnathan <jonnathanospinam@gmail.com>
2022-12-17 01:06:40 +0100

    se sube footer versión beta

--- src/Componentes/Footer.js
+++ src/Componentes/Footer.js
@@ -4,150 +4,45 @@ class Footer extends Component{
     render(){
         return(
           <React.Fragment>
-            <div className="container my-5">
-            
-            <footer
-                    className="text-center text-lg-start text-white"
-                    >
-              
-              <div className="container p-4 pb-0">
-                
-                <section className="">
-                  
+                <footer className="bg-dark text-white py-5">
+                <div className="container">
                   <div className="row">
-                    
-                    <div className="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
-                      <h6 className="text-uppercase mb-4 font-weight-bold">
-                        Company name
-                      </h6>
-                      <p>
-                        Here you can use rows and columns to organize your footer
-                        content. Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit.
-                      </p>
+                    <div className="col-md-3">
+                      <h5>About Us</h5>
+                      <p>Rent a Car es la compañia lider en alquiler de vehiculos tipo automovil familiar, a traves de tecnología e innovación te brindamos la mejor experiencia del mercado.</p>
                     </div>
-                    
-                    <hr className="w-100 clearfix d-md-none" />
-
-                    
-                    <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
-                      <h6 className="text-uppercase mb-4 font-weight-bold">Products</h6>
-                      <p>
-                        <a href="/" className="text-white">MDBootstrap</a>
-                      </p>
-                      <p>
-                        <a href="/" className="text-white">MDWordPress</a>
-                      </p>
-                      <p>
-                        <a className="text-white">BrandFlow</a>
-                      </p>
-                      <p>
-                        <a className="text-white">Bootstrap Angular</a>
-                      </p>
+                    <div className="col-md-3">
+                      <h5>Info</h5>
+                      <ul className="list-unstyled">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Servicios</a></li>
+                        <li><a href="#">PQRS</a></li>
+                      </ul>
                     </div>
-
-                    <hr className="w-100 clearfix d-md-none" />
-
-                    
-                    <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
-                      <h6 className="text-uppercase mb-4 font-weight-bold">
-                        Useful links
-                      </h6>
-                      <p>
-                        <a className="text-white">Your Account</a>
-                      </p>
-                      <p>
-                        <a className="text-white">Become an Affiliate</a>
-                      </p>
-                      <p>
-                        <a className="text-white">Shipping Rates</a>
-                      </p>
-                      <p>
-                        <a className="text-white">Help</a>
-                      </p>
+                    <div className="col-md-3">
+                      <h5>Social</h5>
+                      <ul className="list-unstyled">
+                        <li><a href="#">Facebook</a></li>
+                        <li><a href="#">Twitter</a></li>
+                        <li><a href="#">Instagram</a></li>
+                      </ul>
                     </div>
-
-                    
-                    <hr className="w-100 clearfix d-md-none" />
-                    
-                      <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
-                      <h6 className="text-uppercase mb-4 font-weight-bold">Contact</h6>
-                      <p><i className="fas fa-home mr-3"></i> New York, NY 10012, US</p>
-                      <p><i className="fas fa-envelope mr-3"></i> info@gmail.com</p>
-                      <p><i className="fas fa-phone mr-3"></i> + 01 234 567 88</p>
-                      <p><i className="fas fa-print mr-3"></i> + 01 234 567 89</p>
+                    <div className="col-md-3">
+                      <h5>Contact</h5>
+                      <p>742 Evergreen Terrace<br/>
+                      Bogotá, COL 14325</p>
                     </div>
-                  
                   </div>
-                  
-                </section>
-                
-
-                <hr className="my-3" />
-
-                
-                <section className="p-3 pt-0">
-                  <div className="row d-flex align-items-center">
-                
-                    <div className="col-md-7 col-lg-8 text-center text-md-start">
-                
-                      <div className="p-3">
-                        © 2020 Copyright:
-                        <a className="text-white" href="https://mdbootstrap.com/"
-                          >MDBootstrap.com</a
-                          >
-                      </div>
-                      
+                  <div className="row mt-5">
+                    <div className="col-md-12">
+                      <p className="text-center">Copyright &copy; 2022 Rent a Car</p>
                     </div>
-                    
-
-                    
-                    <div className="col-md-5 col-lg-4 ml-lg-0 text-center text-md-end">
-                    
-                      <a
-                        classNameName ="btn btn-outline-light btn-floating m-1"
-                        className="text-white"
-                        role="button"
-                        ><i className="fab fa-facebook-f"></i
-                        ></a>
-
-                    
-                      <a
-                        classNameame ="btn btn-outline-light btn-floating m-1"
-                        className="text-white"
-                        role="button"
-                        ><i className="fab fa-twitter"></i
-                        ></a>
-
-                      
-                      <a
-                        classNameName ="btn btn-outline-light btn-floating m-1"
-                        className="text-white"
-                        role="button"
-                        ><i className="fab fa-google"></i
-                        ></a>
-
-                      <a
-                        classNameName ="btn btn-outline-light btn-floating m-1"
-                        className="text-white"
-                        role="button"
-                        ><i className="fab fa-instagram"></i
-                        ></a>
-                    </div>
-                    
                   </div>
-                </section>
-                
-              </div>
-            </footer>
-            
-          </div>
-          
+                </div>
+              </footer>
+
+                        
           </React.Fragment>
-                    
-          
-
-
         );
     }
 }
