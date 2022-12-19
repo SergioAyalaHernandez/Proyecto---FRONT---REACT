856579d9f018cddaba5936216d45c8f844dd16ea
Sergio Ayala <sergio.ayala@aldeamo.com>
2022-12-19 12:40:35 -0500

    a

---
+++ src/Componentes/AgregarUsuarios.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class AgregarUsusarios extends Component{
+    render(){
+        return(
+            //<React.Fragment></React.Fragment> <----- esto es opcional cuando no se requiere etiqueta
+            <div>
+                <h1>prros agregando Usuarios</h1>     
+            </div>
+            );      
+    }    
+}
+
+export default AgregarUsusarios;
